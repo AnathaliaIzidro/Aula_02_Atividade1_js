2204198254250b025debf9c5547dd8b9cc51a543
Anathalia Izidro <49588396+AnathaliaIzidro@users.noreply.github.com>
2024-07-18 14:06:03 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+var N1 = prompt("Digite um número."); 
+var valor = N1/2; 
+if(N1 % 2 == 0) {
+    alert("Par"); 
+} 
+if(N1 % 2 == 1){
+    alert("Impar"); 
+}
+resultado = (N1 = (N1 % 2 == 0) && N1 >=0 ) 
+console.log("Para essa operação o resultado é", resultado)
